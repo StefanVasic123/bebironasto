2cc020f38e07a7bdb9c8f96298839235d7fe5d55
Stefan Vasic <vasicstefan123@gmail.com>
2021-02-12 11:17:22 +0100

    v-2-chat-1

--- client/src/components/subheader/chat/Chat.js
+++ client/src/components/subheader/chat/Chat.js
@@ -16,7 +16,7 @@ const Chat = ({ location }) => {
     const [messages, setMessages] = useState([]);
     const name = localStorage.getItem('name');
 
-    const ENDPOINT = 'localhost:5000';
+    const ENDPOINT = 'https://bebironasto.herokuapp.com/';
 
     useEffect(() => {
         const { room } = queryString.parse(location.search); // get room from http query
